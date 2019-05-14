6163efe0f121e444e3e6dc930690420043d5df2e
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-14 16:29:35 -0500

    Se agrego la libreria de estilos de boostrap

--- src/index.php
+++ src/index.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Index</title>
+    <link rel="stylesheet" href="./css/bootstrap.css">
 </head>
 <body>
-    <main>
+    <main class="container">
         <header>
             <h1>Login</h1>          
         </header>
