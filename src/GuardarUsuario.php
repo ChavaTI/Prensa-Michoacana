9cd758d85a5dd9a43cc3055bf57f9787ea721c37
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-17 10:41:44 -0500

    Se mandan los datos a guardar del nuevo a usuario por metodo POST a GuardarUsuario.php

---
+++ src/GuardarUsuario.php
@@ -0,0 +1,9 @@
+<?php
+  $nombre = $_POST['nombre'];  
+  $pass = $_POST['pass'];
+  $email = $_POST['email'];
+  
+  echo "El nombre de usuario es :" . $nombre . "<br>";
+  echo "La contraseña de usuario es :" . $pass . "<br>";
+  echo "El email de usuario es :" . $email . "<br>";
+?>
