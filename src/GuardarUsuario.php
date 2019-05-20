560345801eeddf44dc324da201f2f171e78012af
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-20 15:19:53 -0500

    Se agrego la conexion a la base de datos desde GuardarUsuario.php

--- src/GuardarUsuario.php
+++ src/GuardarUsuario.php
@@ -3,7 +3,5 @@
   $pass = $_POST['pass'];
   $email = $_POST['email'];
   
-  echo "El nombre de usuario es :" . $nombre . "<br>";
-  echo "La contraseña de usuario es :" . $pass . "<br>";
-  echo "El email de usuario es :" . $email . "<br>";
+  $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
 ?>
