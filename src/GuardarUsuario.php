e31e91ee1afacc18505eeaf4d4b5969367496405
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-20 15:29:48 -0500

    Insercion de datos al 50 porciento

--- src/GuardarUsuario.php
+++ src/GuardarUsuario.php
@@ -1,7 +1,24 @@
 <?php
+
+//----------------Variables que se resiven por metodo POST------------------
   $nombre = $_POST['nombre'];  
   $pass = $_POST['pass'];
   $email = $_POST['email'];
-  
+//---------------------------------------------------------------------------
+
+//---------------Coneccion a la base de  datos-------------------------------
   $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+//---------------------------------------------------------------------------
+
+//---------------Si hay un error en la conexion avisar al usuario-----------
+  if($conn->connect_errno){
+    echo 'Error: Hubo un error al conectar la base de datos \n';
+    echo 'Error: ' .$conn->connect_errno;
+    exit;
+  }
+//----------------------------------------------------------------------------
+
+//----------------insertar los datos en la tabla ------------------------------
+  $sql = '';
+//-----------------------------------------------------------------------------
 ?>
