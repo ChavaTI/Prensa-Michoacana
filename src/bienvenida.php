53b9838be8133b998e2f33ae1dba9d1ae0f1119f
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-21 16:17:50 -0500

    Login en un cincenta por ciento

--- src/bienvenida.php
+++ src/bienvenida.php
@@ -1,1 +1,30 @@
-Hola Funciono !!!
+<?php
+
+//------------------------Resivir las variables por metodo POST----------------------
+$email = $_POST['nombre'];
+$pass = $_POST['pass'];
+//------------------------------------------------------------------------------------
+
+//----------------Encriptar contraseña--------------------------------------
+$passEncryp = hash('sha256', 'pass'.$pass );
+//--------------------------------------------------------------------------
+
+//---------------Coneccion a la base de  datos-------------------------------
+$conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+//---------------------------------------------------------------------------
+
+//---------------Si hay un error en la conexion avisar al usuario-----------
+if($conn->connect_errno){
+    echo 'Error: Hubo un error al conectar la base de datos \n';
+    echo 'Error: ' .$conn->connect_errno;
+    exit;
+  }
+//----------------------------------------------------------------------------
+
+
+//------------seleccionar el registro con el email que este por post----------------
+$sql = '';
+
+//-----------------------------------------------------------------------------------
+
+?>
