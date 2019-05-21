5aab446035915506ce34f280d43a32ec5a5fd10a
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-21 16:54:59 -0500

    Login realizado con exito

--- src/bienvenida.php
+++ src/bienvenida.php
@@ -23,8 +23,16 @@ if($conn->connect_errno){
 
 
 //------------seleccionar el registro con el email que este por post----------------
-$sql = 'SELECT * FROM `Usuario` WHERE Status=true AND IdLogin=1;  ';
+$sql = 'SELECT * FROM `Usuario` WHERE Status=true AND Email = "'.$email.'" AND Pass = "'.$passEncryp.'" ';
 
+$result = $conn->query($sql);
+
+if($result->num_rows > 0){
+    $row = $result->fetch_assoc();
+    echo "Hola ".$row['NombreUsuario']." bienvenido a prensa Michoacana tu email es ".$row['Email'];
+}else{
+    echo "No hay nada";
+}
 //-----------------------------------------------------------------------------------
 
 ?>
