0ad467f2b7a1d0eacafee170b52e0a9f0066d968
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-05-28 16:50:25 -0500

    Redireccion hacia atras cunaod se le da cancelar en crearUsuario.html NOTA: Se creo con codigo en JavaScript

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+//-------------este es el codigo para crearUsuario.html-----------------
+let botonCancelar = document.getElementById('btnCancelar');
+
+botonCancelar.addEventListener('click',()=>{
+    console.log("Estas Entrado?");
+    location.href="http://localhost:9090/index.html";
+});
+
+//-----------------------------------------------------------------------
