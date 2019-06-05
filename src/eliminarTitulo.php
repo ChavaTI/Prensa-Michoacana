22577e5f89b35385eaddc8b5e889baa575d434f9
Salvador Hernández López <salvador@thesoftwareartisans.com>
2019-06-05 16:34:19 -0500

    Logica de borrado de titulo casi terminado

---
+++ src/eliminarTitulo.php
@@ -0,0 +1,28 @@
+<script>
+ var confirmacion  = confirm("¿Esta usted seguro de que desea eliminar el titulo?");
+ console.log(confirmacion);
+ if(confirmacion == false){
+    location.href="http://localhost:9090/bienvenida.php";
+ }
+</script>
+
+<?php 
+    echo $_GET['IdTitulo'];
+
+    $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+    if($conn->connect_errno){
+        echo 'Error: Hubo un error al conectar la base de datos \n';
+        echo 'Error: ' .$conn->connect_errno;
+        exit;
+      }
+    $sql = '';
+
+    if($conn->query($sql)){
+        header('Location: ./bienvenida.php');
+      }else{
+        echo "Error: Problemas al crear un nuevo titulo .... ".$conn->error;
+    }
+
+    $conn->close();
+
+?>
