bb67b2bbbb15ff8ffc682b13857ad95d41648cf9
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-05 14:38:47 -0500

    intente añadir el formulario para actualizarTitulo.php sin embargo me marca error en la linea 25, intente varias formas pero no tuve exito

--- src/ActualizarTitulo.php
+++ src/ActualizarTitulo.php
@@ -1,3 +1,39 @@
 <?php
 echo "Prueba exitosa!!";
 ?>
+<?php
+
+//----------------Variables que se reciben por metodo POST------------------
+  $nombre = $_POST['Nombre'];  
+  $estado= $_POST['Estado'];
+  $ciudad = $_POST['Ciudad'];
+  $periodicidad = $_POST['Periodicidad'];
+  $responsable = $_POST['Responsable'];
+  $numerodepaginas = $_POST['Numerodepaginas'];
+  $orientacionymedidas = $_POST['Orientacionymedidas'];
+  $IdTitulo = $_POST['IdTitulo'];
+ //---------------Conexion a la base de  datos-------------------------------
+ $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+
+ //---------------Si hay un error en la conexion avisar al usuario-----------
+ if($conn->connect_errno){
+   echo 'Error: Hubo un error al conectar la base de datos \n';
+   echo 'Error: ' .$conn->connect_errno;
+   exit;
+
+   //----------------insertar los datos en la tabla ------------------------------
+  $sql= ( UPDATE `Titulo` SET `Nombre`= '.$nombre.',`Estado`= '.$estado.',`Ciudad`='.$ciudad.',`Periodicidad`='.$periodicidad.',`Responsable`='.$responsable.',`NoPaginas`='.$numerodepaginas.',`OrientacionYMedidas`='.$orientacionymedidas.' WHERE 'IdTitulo'='.$IdTitulo.' = 2 );
+
+  if($conn->query($sql)){
+   header('Location: ./bienvenida.php');
+  
+  }else{
+    echo "Error: Problemas al crear un nuevo titulo .... ".$conn->error;
+}
+
+//-----------------------------------------------------------------------------
+
+//-------------------------cerrar conexion------------------------------------
+$conn->close();
+//------------------------------------------------------------------------------
+?> 
