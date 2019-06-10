96cf9ae16f4d8ae9ad214fe89624143f68831f57
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-10 16:03:26 -0500

    se añadio el guardar tomo

--- src/controllers/GuardarTomo.php
+++ src/controllers/GuardarTomo.php
@@ -1,4 +1,22 @@
 <?php
+$numTomo = $_POST['numTomo']; 
+$idTitulo = $_POST['idTitulo']; 
 
+$conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+
+if($conn->connect_errno){
+    echo 'Error: Hubo un error al conectar la base de datos \n';
+    echo 'Error: ' .$conn->connect_errno;
+    exit;
+}
+
+$sql='INSERT INTO Tomo (NumeroTomo, IdTitulo, Status) VALUES ('.$numTomo.','.$idTitulo.', 1)';
+if($conn->query($sql)){
+    header('location: ../Titulo.php');
+
+}else{
+    echo "Error: Problemas al crear un nuevo Tomo .... ".$conn->error;
+}
+$conn->close();
 
 ?>
