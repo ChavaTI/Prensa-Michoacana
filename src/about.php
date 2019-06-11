c25fa05c8695b6628da80fe52a06971dd86cdc7a
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-11 16:09:38 -0500

    Se intento darle un diseño al about.html sin tener exito

--- src/about.php
+++ src/about.php
@@ -12,8 +12,8 @@ if($_SESSION['IdLogin'] == null){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./css/fonts.css">
-    <link rel="stylesheet" href="./css/style.css">
+   
+    
     <title>Acerca de la Hemeroteca Digital de Ciencia y Prensa Michoacana</title>
 </head>
 <body>
