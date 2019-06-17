26e1dc4ca931f58d1b344bf7493a0f907e072f9b
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-17 16:04:09 -0500

    se añadio el php de elominar Tomo

--- src/controllers/eliminarTomo.php
+++ src/controllers/eliminarTomo.php
@@ -1,3 +1,18 @@
 <?php
+    $conn = new mysqli('db','administrator','adminpass1223','PrensaMichoacana');
+    if($conn->conect_errno){
+        echo 'Error:Hubo un error al conectar la base de datos \n';
+        echo 'Error: ' .$conn->connect_errno;
+        exit;
+    }
+    $sql = 'UPDATE `Tomo` SET `Status`=false WHERE IdTomo='.$_GET['IdTomo'];
+    
+    if($conn->query($sql)){
+        header('Location: ../Titulo.php');
+      }else{
+            echo "Error: Problemas al crear un nuevo tomo ....".$conn->error;
+    }
+
+    $conn->close();
 
 ?>
