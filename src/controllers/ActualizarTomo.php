ea8d5fff321c12b531555141456bc019933a6edd
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-18 16:03:02 -0500

    se termino el ActualizarTomo.php, en espera de correcciones

--- src/controllers/ActualizarTomo.php
+++ src/controllers/ActualizarTomo.php
@@ -2,7 +2,7 @@
 $numTomo = $_POST['numTomo']; 
 $idTitulo = $_POST['idTitulo']; 
 $Nombre = $_POST['NombreTitulo'];
-
+$idTomo = $_POST['idTomo'];
 $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
 
 if($conn->connect_errno){
@@ -11,12 +11,12 @@ if($conn->connect_errno){
     exit;
 }
 
-$sql='UPDATE `Tomo` SET `NumeroTomo`= $_POST['numTomo']; ,`IdTitulo`=1 WHERE IdTomo=1;
+$sql='UPDATE `Tomo` SET `NumeroTomo`= '.$numTomo.', WHERE IdTomo= '.$idTomo.';
 if($conn->query($sql)){
     header('location: ../Titulo.php?IdTitulo='.$idTitulo.'&NombreTitulo='.$Nombre);
 
 }else{
-    echo "Error: Problemas al crear un nuevo Tomo .... ".$conn->error;
+    echo "Error: Problemas al actualizar un nuevo Tomo .... ".$conn->error;
 }
 $conn->close();
 
