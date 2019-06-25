123ecf2a480d6b1de39314fbdaaba88872ba6799
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-06-25 17:08:14 -0500

    se hicieron correcciones de sintaxis al codigo para que el visual studio no marcara errores

--- src/Titulo.php
+++ src/Titulo.php
@@ -42,10 +42,10 @@ if ($_SESSION['IdLogin'] == null) {
                 $conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
                 //---------------------------------------------------------------------------
                 //---------------Si hay un error en la conexion avisar al usuario-----------
-                if($conn->connect_errno){
-                echo 'Error: Hubo un error al conectar la base de datos \n';
-                echo 'Error: ' .$conn->connect_errno;
-                exit;
+                if ($conn->connect_errno) {
+                    echo 'Error: Hubo un error al conectar la base de datos \n';
+                    echo 'Error: ' .$conn->connect_errno;
+                    exit;
                 }
                 //---------------seleccionar los titulos--------------------------------------
                 $sql = 'SELECT * FROM `Tomo` WHERE Status=true AND IdTitulo ='.$_GET['IdTitulo'].'' ;
