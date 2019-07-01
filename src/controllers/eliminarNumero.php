397faea8f111f71d624a13e5019d123e4cb0581b
Jennifer Melgoza Tellez <jennifer@thesoftwareartisans.com>
2019-07-01 15:00:24 -0500

    hice el codigo de eliminarNUmero.php pero no logro hacer que se redireccione de forma correcta

--- src/controllers/eliminarNumero.php
+++ src/controllers/eliminarNumero.php
@@ -1,3 +1,19 @@
 <?php
-echo 'Hola mundo';
+
+$conn = new mysqli('db','administrator','adminpass123','PrensaMichoacana');
+if ($conn->conect_errno){
+    echo 'Error:Hubo un error al conectar la base de datos \n';
+    echo 'Error: ' .$conn->connect_errno;
+    exit;
+}
+$sql = 'UPDATE `Numero` SET `Status`=false WHERE IdNumero='.$_GET['IdNumero'];
+
+if ($conn->query($sql)){
+    header('Location: ../Tomo.php?IdTomo='.$_GET['IdTomo'].'&NumeroTomo='.$_GET['NumeroTomo']. '&NombreTitulo=' .$_GET['NombreTitulo']);
+  }else{
+        echo "Error: Problemas al eliminar un número ....".$conn->error;
+}
+
+$conn->close();
+
 ?>
